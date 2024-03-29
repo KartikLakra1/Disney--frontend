176c02a6ae860ce4d97ec91da3895b5a760b8c45
KartikLakra1 <kitrakarkal@gmail.com>
2024-03-29 18:02:06 +0530

    componenets added

--- src/Componets/GenreMovieList.jsx
+++ src/Componets/GenreMovieList.jsx
@@ -5,13 +5,14 @@ import MovieList from './MovieList'
 function GenreMovieList() {
   return (
     <div>
-        {GenresList.genere.map((item,index)=>index<=4&&(
-            <div className='p-8 px-8 md:px-16'>
-                <h2 className='text-[20px] text-white 
-                font-bold'>{item.name}</h2> 
-                <MovieList genreId={item.id} index_={index} />   
-            </div>
-        ))}
+      {GenresList.genere.map((item, index) => index <= 4 && (
+
+        <div className='p-8 px-8 md:px-16'>
+          <h2 className='text-[20px] text-white 
+                font-bold'>{item.name}</h2>
+          <MovieList genreId={item.id} index_={index} />
+        </div>
+      ))}
     </div>
   )
 }
