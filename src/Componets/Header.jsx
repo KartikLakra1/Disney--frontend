83dec1c7260a662bb3051a020642a197f606f57e
KartikLakra1 <kitrakarkal@gmail.com>
2024-06-25 19:38:21 +0530

    minor ui changes

--- src/Componets/Header.jsx
+++ src/Componets/Header.jsx
@@ -41,21 +41,21 @@ function Header() {
         <div className='flex items-center justify-between p-5'>
             <div className='flex  gap-8 items-center'>
                 <img src={logo} className='w-[80px] 
-        md:w-[115px] object-cover' />
+        md:w-[115px] object-cover bg-center bg-cover' />
 
                 <div className='hidden md:flex gap-8'>
                     {menu.map((item) => (
                         <HeaderItem name={item.name} Icon={item.icon} />
                     ))}
                 </div>
-                <div className='flex md:hidden gap-5'>
+                <div className='flex md:hidden gap-6'>
                     {menu.map((item, index) => index < 3 && (
                         <HeaderItem name={''} Icon={item.icon} />
                     ))}
                     <div className='md:hidden' onClick={() => setToggle(!toggle)}>
                         <HeaderItem name={''} Icon={HiDotsVertical} />
                         {toggle ? <div className='absolute mt-3 bg-[#121212] 
-            border-[1px] border-gray-700 p-3 px-5 py-4'>
+            border-[1px] border-gray-700 p-3 px-4 py-4'>
                             {menu.map((item, index) => index > 2 && (
                                 <HeaderItem name={item.name} Icon={item.icon} />
                             ))}
